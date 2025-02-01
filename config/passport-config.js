bd2eea27ebe484d507cc91e7e934dd1bf8c8186f
Theophilus Thobejane <Thobejane.theophiles@gmail.com>
2025-02-01 22:51:32 +0200

    /deployment debug

--- config/passport-config.js
+++ config/passport-config.js
@@ -27,7 +27,9 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "https://node-authentication-q79o.onrender.com/auth/google/callback",
+            // callbackURL: "https://node-authentication-q79o.onrender.com/auth/google/callback",
+            callbackURL: "https://node-authentication-fx4o.onrender.com/auth/google/callback",
+            
         },
         (token, tokenSecret, profile, done) => {
             const email = profile.emails[0].value;
